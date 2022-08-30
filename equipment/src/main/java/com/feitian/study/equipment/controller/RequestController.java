80e62e084438a08f02dfe5268a07d76b9043f431
Zhaotianjun <2071486302@qq.com>
2022-08-30 11:34:19 +0800

    添加接口：获取所有的数据

--- equipment/src/main/java/com/feitian/study/equipment/controller/RequestController.java
+++ equipment/src/main/java/com/feitian/study/equipment/controller/RequestController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author : Zhaotianjun
  * @version : 1.0.0
- * @createdate : 2022/08/30 11:24
  * @doc : 请求控制器
+ * @createdate : 2022/08/30 11:31
  */
 
 @RestController
@@ -18,13 +18,25 @@ public class RequestController {
     /**
      * @param name 名字
      * @return {@link String }
-     * @createdate : 2022/08/30 11:25
-     * @author : Zhaotianjun
      * @doc : 你好
+     * @createdate : 2022/08/30 11:31
+     * @author : Zhaotianjun
      */
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
         return name;
+    }
+
+
+    /**
+     * @return {@link String }
+     * @doc : 得到所有日期
+     * @createdate : 2022/08/30 11:33
+     * @author : Zhaotianjun
+     */
+    @GetMapping("/getAllDate")
+    public String getAllDate() {
+        return "all data";
     }
 
 }
